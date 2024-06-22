87e665dca2ce0bad141b104fe71884992d1f48b2
rohitpandit2003 <r.pandit@op.iitg.ac.in>
2024-06-22 10:10:23 +0530

    Added a code which convert infix expression into postfix expression

--- infix_to_postfix.c
+++ infix_to_postfix.c
@@ -1,112 +1,94 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int isempty(int *top) {
-    if (*top == -1)
-        return -1;
+char operator[7] = {'+', '-', '*', '/', '(', ')', '\0'};
+char input[100];
+char stack[30];
+int top = -1;
+char postfix[100];
+
+int isEmpty() {
+    if (top == -1)
+        return 1;  // Return 1 if stack is empty
     else
-        return 0; // Ensure to return 0 when stack is not empty
+        return 0;  // Return 0 if stack is not empty
 }
 
-char pop(char stack[], int *top) {
-    if (*top == -1) // Check if stack is empty before popping
-        return '\0'; // Return a default value or handle error appropriately
-    char el = stack[*top];
-    stack[*top] = '\0';
-    (*top)--;
-    return el;
+int precedence(char curr_char) {
+    if (curr_char == operator[0] || curr_char == operator[1]) // if curr_char = + or - then it return 0  
+        return 0;
+    else if (curr_char == operator[2] || curr_char == operator[3])  // else if curr_char = * or /  then it returns 1
+        return 1;
+    else if (curr_char == operator[4]) // else if curr_char = openbracket then it returns 2
+        return 2;
+    else if (curr_char == operator[5]) // else if curr_char = closing bracket then it returns 3
+        return 3;
+    else
+        return -1; // if it is not anything above it means curr_char is operand.
 }
 
-void push(char stack[], char el, int *top) {
-    (*top)++;
-    stack[*top] = el;
+void push(char curr_char) { // push function insert the operators in the top of stack 
+    top++;
+    stack[top] = curr_char;
+}
+
+char pop() {  // pop function removes the operator from the top of stack and return the removed element
+    if (isEmpty() == 1)
+        return '\0';  // Return null character if stack is empty
+    else {
+        char el = stack[top];
+        stack[top] = '\0';
+        top--;
+        return el;
+    }
 }
 
 int main() {
-    char operator[7] = {'+', '-', '*', '/', '(', ')', '\0'};
-
-    char input[100];
-    printf("Write your expression: ");
-    fgets(input, sizeof(input), stdin);
-
-    char stack[100];
-    int top = -1;
-
-    char postfix[100];
-
     int index = 0;
-    for (int i = 0; i < strlen(input); i++) {
-        char current_char = input[i];
-        int isoperator = 0; // isoperator checks whether the input[i] is operator or not, if it is 0 it means it is not an operator and if it is 1 it is an operator.
+    printf("Enter the expression: ");
+    fgets(input, sizeof(input), stdin);  // It takes input from the user
 
-        if (i == (strlen(input) - 1)) {
-            while (top != -1) {
-                postfix[index] = pop(stack, &top);
+    for (int i = 0; i < strlen(input); i++) {  // i created a loop to scan all element of input, and it scans till the length of input
+        char curr_char = input[i];       // current character stores the elment of input at i index
+        
+        if(i == strlen(input)-1){  // if i =  strlen(input)-1 it pops all the element of the stack, it means if the scanning is completed no element left in the input it pop all the element of the stack in the postfix array
+            while(isEmpty()!=1){
+                postfix[index] = pop();
                 index++;
             }
-            break;
         }
 
-        for (int j = 0; j < strlen(operator); j++) {
-            if (current_char == operator[j]) {
-                isoperator++;
-                break;
-            }
-        }
-
-        if (isoperator == 0) {
-            postfix[index] = current_char;
+        if (precedence(curr_char) == -1) { // if precedence(curr_char) returns -1 it means it is operand, and operand stores in the postfix array.
+            postfix[index] = curr_char;
             index++;
-        } else if (current_char == operator[4] || current_char == operator[5]) {
-            if (current_char != operator[5]) {
-                if (isempty(&top) == -1) {
-                    push(stack, current_char, &top);
-                } else {
-                    if (stack[top] == operator[0] || stack[top] == operator[1]) {
-                        if (current_char == operator[0] || current_char == operator[1]) {
-                            postfix[index] = pop(stack, &top);
-                            index++;
-                            push(stack, current_char, &top);
-                        } else {
-                            push(stack, current_char, &top);
-                        }
-                    } else if (stack[top] == operator[2] || stack[top] == operator[3]) {
-                        postfix[index] = pop(stack, &top);
-                        index++;
-                        push(stack, current_char, &top);
-                    }
-                }
-            } else if (current_char == operator[5]) {
-                while (stack[top] != operator[4]) {
-                    postfix[index] = pop(stack, &top);
-                    index++;
-                }
-                pop(stack, &top); // Pop the '(' from the stack, but not stored in postfix.
+        } else if (isEmpty() == 1) { // if stack is empty and precedence(curr_char) is operator than it push the operator in the stack
+            push(curr_char);
+        } else if ((isEmpty() == 0 && stack[top] == operator[4]) || (isEmpty() == 0 && precedence(curr_char) > precedence(stack[top]) && curr_char != operator[5])) { 
+  //in this condition i have taken 2 condition in or basis means one  should be true to satisfy whole condition, first condition says if stack is not empty and stack[top] = opening bracket than push the operator, because we know the opening bracket is higher precedence than other operator, so if stack[top] has opening bracket, we cannot able to push the element. Second condition is " if stack is not empty, and precedence(current character) > precedence(stack[top]) and current character should not be closing bracket than push the element, that condition means if current character has more precedence than the stack[top] element, than we need to push the element in the stack, but we also need to keep in mind about closing bracket, because we know closing bracket is also has higher precedence, so we don't want to push closing bracket in the stack.
+            push(curr_char);
+        } else if (isEmpty() == 0 && curr_char == operator[5]) {
+// in this condition if current character found that it is closing bracket and top is not empty than it pop all the element from the stack and push into postfix array
+            while (stack[top] != operator[4]) {
+                postfix[index] = pop();
+                index++;
             }
-        } else {
-            if (isempty(&top) == -1) {
-                push(stack, current_char, &top);
-            } else {
-                if (stack[top] == operator[0] || stack[top] == operator[1]) {
-                    if (current_char == operator[0] || current_char == operator[1]) {
-                        postfix[index] = pop(stack, &top);
-                        index++;
-                        push(stack, current_char, &top);
-                    } else {
-                        push(stack, current_char, &top);
-                    }
-                } else if (stack[top] == operator[2] || stack[top] == operator[3]) {
-                    postfix[index] = pop(stack, &top);
-                    index++;
-                    push(stack, current_char, &top);
-                }
-            }
+            pop();
+        } else if (isEmpty() == 0 && precedence(curr_char) <= precedence(stack[top])) {
+// in this condition if stack is not empty and precedence of current character is less than or equal to  precedence of stack top, we need to remove top element of stack and push into postfix array.
+            postfix[index] = pop();
+            index++;
+            push(curr_char);
         }
     }
 
-    postfix[index] = '\0'; // Ensure postfix expression is null-terminated
-    puts(postfix);
+   
 
+    postfix[index] = '\0'; // we close the postfix index.
+
+    printf("The postfix expression is:  %s\n", postfix);
+
+    
     return 0;
 }
+	
+	
